619b9569db15fcefffd4d4021d9b9485bc691aa7
Jeff Pugh <39938643+PUGHLABS@users.noreply.github.com>
2026-01-30 21:54:33 -0800

    Remove mobile menu toggle functionality

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const menu = document.querySelector('#mobile-menu');
-const navList = document.querySelector('#nav-list');
 
-menu.addEventListener('click', () => {
-    navList.classList.toggle('active');
-});
